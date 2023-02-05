6333c2ccb95a584814e2eb6a42278507e994e828
Kaio Batista <kaiobatist@gmail.com>
2023-02-05 13:40:21 -0300

    Initial Commit

---
+++ script.js
@@ -0,0 +1,7 @@
+for (const element of document.querySelectorAll(".botao-converter")) {
+    element.addEventListener("click", convert)
+}
+
+function convert(e) {
+    console.log("Hello world!")
+}
